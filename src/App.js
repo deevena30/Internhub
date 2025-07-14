9e99552fd073474d14450584175a1b6aaaa4fdb8
Name <undrathi.deevena@gmail.com>
2025-07-14 14:58:08 +0000

    internhub

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <main style={{ marginLeft: 240, width: 'calc(100vw - 240px)' }}>
+      <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/resumes" element={<ResumeBank />} />
